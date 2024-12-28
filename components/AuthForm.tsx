f9c0457dc154ad19946a9c0ad03c60a9ddbfbafb
Divy1030 <157683952+Divy1030@users.noreply.github.com>
2024-12-28 18:55:04 +0530

    implement AuthForm component for sign-in and sign-up pages; create MyBanks, Transfer, and TransactionHistory pages

---
+++ components/AuthForm.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const AuthForm = ({type}:{type:string}) => {
+    const [user, setuser] = useState(null)
+  return (
+    <section className='auth-form'>
+        <header className='flex flex-col gap-5 md:gap-8'>
+        <Link href="/" className="cursor-pointer flex items-center gap-1">
+            <Image 
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Horizon logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
+          </Link>
+          <div className='flex flex-col gap-1 md:gap-3'> 
+            <h1 className='text-24 lg:text-36 fonr-semibold text-gray-900'>
+                {user ? 'Link Account':type==='sign-in'
+                ?'Sign In':'Sign Up'}
+                <p className='text-16 font-normal text-gray-600'>
+                    {user ? 'Link your account to Horizon':'Please enter your credentials'}
+                </p>
+            </h1>
+          </div>
+        </header>
+        {user ? (
+            <div className='flex flex-col gap-4'>
+                {/* {PlaidLink} */}
+            </div>
+        ):(
+            <>
+            Form
+            </>
+        )}
+    </section>
+  )
+}
+
+export default AuthForm
